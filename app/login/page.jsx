6df7ef4d44881fdc83df49157a660c352ed8e317
qauser.javaid00 <qaiser.javaid00@gmail.com>
2026-05-11 00:16:04 +0500

    initial commit

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 
 export default function LoginPage() {
     return (
-        <PublicOnlyRoute>
-            <div className="min-h-screen flex items-center justify-center bg-gray-50">
-                <div className="bg-white p-6 rounded-2xl shadow w-80">
-                    <h1 className="text-xl font-semibold mb-4 text-center">
-                        TaskBoard Login
-                    </h1>
+        // <PublicOnlyRoute>
+        <div className="min-h-screen flex items-center justify-center bg-gray-50">
+            <div className="bg-white p-6 rounded-2xl shadow w-80">
+                <h1 className="text-xl font-semibold mb-4 text-center">
+                    TaskBoard Login
+                </h1>
 
-                    <LoginForm />
+                <LoginForm />
 
-                    <p className="text-sm text-center mt-4">
-                        Don’t have an account?{" "}
-                        <Link href="/register" className="text-blue-600">
-                            Register
-                        </Link>
-                    </p>
-                </div>
+                <p className="text-sm text-center mt-4">
+                    Don’t have an account?{" "}
+                    <Link href="/register" className="text-blue-600">
+                        Register
+                    </Link>
+                </p>
             </div>
-        </PublicOnlyRoute>
+        </div>
+        // </PublicOnlyRoute>
     );
 }
