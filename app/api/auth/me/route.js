0f902222cbd8ff4fac92edf3715c35f926cc6ae7
qauser.javaid00 <qaiser.javaid00@gmail.com>
2026-05-11 00:22:42 +0500

    initial commit

--- app/api/auth/me/route.js
+++ app/api/auth/me/route.js
@@ -2,29 +2,35 @@ import { NextResponse } from "next/server";
 import { verifyToken } from "@/lib/auth";
 import { cookies } from "next/headers";
 
-export const dynamic = "force-dynamic";
 
-export async function GET() {
+function getUserFromReq() {
     const token = cookies().get("token")?.value;
 
-    if (!token) {
+    if (!token) return null;
+
+    try {
+        const user = verifyToken(token);
+
+        return user;
+    } catch (e) {
+        console.log("JWT ERROR:", e.message);
+        return null;
+    }
+}
+
+
+export async function GET() {
+    const user = getUserFromReq();
+
+    if (!user) {
         return NextResponse.json(
             { message: "Unauthorized" },
             { status: 401 }
         );
     }
 
-    try {
-        const user = verifyToken(token);
-
-        return NextResponse.json(
-            { user },
-            { status: 200 }
-        );
-    } catch (e) {
-        return NextResponse.json(
-            { message: "Invalid token" },
-            { status: 401 }
-        );
-    }
+    return NextResponse.json(
+        { user },
+        { status: 200 }
+    );
 }
