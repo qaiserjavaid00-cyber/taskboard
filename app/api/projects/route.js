4e1bc59a33619f253f171125a21701c1593edcd4
qauser.javaid00 <qaiser.javaid00@gmail.com>
2026-05-11 18:01:33 +0500

    updated UI

--- app/api/projects/route.js
+++ app/api/projects/route.js
@@ -1,5 +1,6 @@
 import { connectDB } from "@/lib/db";
 import Project from "@/lib/models/Project";
+import User from "@/lib/models/User";
 import { NextResponse } from "next/server";
 import { verifyToken } from "@/lib/auth";
 import { cookies } from "next/headers";
@@ -47,77 +48,6 @@ export async function POST(req) {
     return NextResponse.json(project, { status: 201 });
 }
 
-// export async function GET(req) {
-//     await connectDB();
-
-//     const user = getUserFromReq();
-
-//     if (!user) {
-//         return NextResponse.json(
-//             { message: "Unauthorized" },
-//             { status: 401 }
-//         );
-//     }
-
-//     const projects = await Project.find({ owner: user.userId });
-
-//     return NextResponse.json(projects);
-// }
-
-
-// export async function GET(req) {
-//     try {
-//         await connectDB();
-
-//         const user = getUserFromReq()
-//         if (!user) {
-//             return NextResponse.json(
-//                 { message: "Unauthorized" },
-//                 { status: 401 }
-//             );
-//         }
-//         let projects = await Project.find()
-//             .populate("owner", "name email")
-//             .populate("members", "name email");
-
-//         // 🔥 ADMIN → sees everything
-//         if (user.role === "admin") {
-//             return NextResponse.json({
-//                 ownedProjects: projects,
-//                 teamProjects: [],
-//             });
-//         }
-
-//         // 🔥 OWNED PROJECTS
-//         const ownedProjects = projects.filter(
-//             (p) => p.owner._id.toString() === user.userId
-//         );
-
-//         // 🔥 TEAM PROJECTS
-//         const teamProjects = projects.filter((p) => {
-//             const isOwner =
-//                 p.owner._id.toString() === user.userId;
-
-//             const isMember = p.members.some(
-//                 (m) => m._id.toString() === user.userId
-//             );
-
-//             return !isOwner && isMember;
-//         });
-
-//         return NextResponse.json({
-//             ownedProjects,
-//             teamProjects,
-//         });
-
-//     } catch (err) {
-//         return NextResponse.json(
-//             { message: "Server error" },
-//             { status: 500 }
-//         );
-//     }
-// }
-
 
 export async function GET(req) {
     try {
@@ -137,7 +67,7 @@ export async function GET(req) {
             .populate("owner", "name email")
             .populate("members", "name email");
 
-        // 🔥 ADMIN
+
         if (user.role === "admin") {
             return NextResponse.json({
                 ownedProjects: projects,
@@ -145,12 +75,12 @@ export async function GET(req) {
             });
         }
 
-        // 🔥 OWNED PROJECTS
+
         const ownedProjects = projects.filter((p) =>
             p.owner?._id?.toString() === user.userId
         );
 
-        // 🔥 TEAM PROJECTS
+
         const teamProjects = projects.filter((p) => {
             const isOwner =
                 p.owner?._id?.toString() === user.userId;
